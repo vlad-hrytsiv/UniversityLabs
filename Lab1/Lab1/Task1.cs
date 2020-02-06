0baf2abc67b844402d7fb01ad7ec4b4b57b7695e
vlad-hrytsiv <54137412+vlad-hrytsiv@users.noreply.github.com>
2020-02-06 16:59:19 +0200

    Готово перше завдання лабораторної №1

---
+++ Lab1/Lab1/Task1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Task1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
