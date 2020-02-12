152b3cbb21a4d3598b2b0a0c86dbe7dfbfb8da71
vlad-hrytsiv <54137412+vlad-hrytsiv@users.noreply.github.com>
2020-02-12 19:51:17 +0200

    Lab 3 ready

--- Lab3/Task7/Program.cs
+++ Lab3/Task7/Program.cs
@@ -10,6 +10,34 @@ namespace Task7
     {
         static void Main(string[] args)
         {
+            Console.Write("Сколько вложить денег? ");
+            double depositValue = Convert.ToDouble(Console.ReadLine());
+
+            if (depositValue > 2000)
+            {
+                throw new ArgumentException("Сумма вложения не может быть больше чем 2000!");
+            }
+
+            double withdrawUAH, withdrawEUR, withdrawUSD;
+
+            withdrawUAH = depositValue + depositValue * 0.1;
+            withdrawEUR = (depositValue + depositValue * 0.11) * 26.7;
+            withdrawUSD = (depositValue + depositValue * 0.15) * 24.25;
+
+            Console.WriteLine("Если вложить {0} гривен, Вы получите {1} грн.", depositValue, withdrawUAH);
+            Console.WriteLine("Если вложить {0} евро, Вы получите {1} грн.", depositValue, withdrawEUR);
+            Console.WriteLine("Если вложить {0} долларов, Вы получите {1} грн.", depositValue, withdrawUSD);
+
+            if (withdrawUAH > withdrawEUR && withdrawUAH > withdrawUSD)
+            {
+                Console.WriteLine("Самый выгодной вклад в гривне!");
+            } else if  (withdrawEUR > withdrawUAH && withdrawEUR > withdrawUSD)
+            {
+                Console.WriteLine("Самый выгодной вклад в евро!");
+            } else
+            {
+                Console.WriteLine("Самый выгодной вклад в долларах!");
+            }
         }
     }
 }
